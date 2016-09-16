8c01e284a5addab9ba58d1012504ef625b54aabc
kevin caradant <kevin.caradant@gmail.com>
2016-09-16 19:48:11 +0200

    feat: Avoid to many watch for mac and unix system. Exclude the watch on the node_modules folders + exclude the apply of loaders on node_modules

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,31 +1,43 @@
 var path = require('path'),
 webpack = require('webpack'),
 libPath = path.join(__dirname, 'lib'),
+nodeModulesPath = path.join(__dirname, 'node_modules'),
 HtmlWebpackPlugin = require('html-webpack-plugin'),
 ProgressBarPlugin = require('progress-bar-webpack-plugin'), // To be delete when webpack will accept the flag --progress in the devserver and not only in CMD
-sassLintPlugin = require('sasslint-webpack-plugin');
+WatchIgnorePlugin = require('watch-ignore-webpack-plugin'),
+SassLintPlugin = require('sasslint-webpack-plugin');
+//const LIB = path.join(__dirname, 'lib/')
+
 var cf = {
 
 	entry: path.join(libPath, 'index.js'),
 
 	devtool: 'source-map',
+	resolve: {
+		root: [path.resolve(__dirname, 'lib'), path.resolve(__dirname, 'node_modules')],
+		extensions: ['', '.js', '.json', '.scss']
+	},
 	debug: true,
 	cache: false,
 	module: {
 		loaders: [{
 			test: /\.json$/,
+			exclude: /node_modules/,
 			loaders: ["raw-loader"]
 		},{
 			test: /\.html$/,
+			exclude: /node_modules/,
 			loader: 'ngtemplate!html'
 		},{
 			test: /\.(jpe?g|gif|png|svg|woff|woff2|ttf|eot|wav|mp3)$/,
 			loader: 'file' // inline base64 URLs for <=10kb images, direct URLs for the rest
 		},{
 			test: /\.less$/,
+			exclude: /node_modules/,
 			loaders: ['style', 'css', 'less?sourceMap', 'resolve-url']
 		},{
 			test: /\.scss$|\.css$/,
+			exclude: /node_modules/,
 			loaders: ['style', 'css-loader', 'resolve-url', 'sass?sourceMap']
 		},{
 			test: /\.js$/,
@@ -62,7 +74,12 @@ var cf = {
 
 		new webpack.HotModuleReplacementPlugin(),
 
-		new sassLintPlugin({
+		new WatchIgnorePlugin([
+			path.resolve( __dirname, './examples/'),
+			path.resolve(__dirname, './node_modules/'),
+		]),
+
+		new SassLintPlugin({
 			glob: 'lib/**/*.s?(a|c)ss',
 			configFile: '.sass-lint.yml'
 		}),
